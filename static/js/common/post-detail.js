a8ae3adaed53e8096d3e8d432eeaf83e289bbd4f
xiongwilee <xiongwilee@foxmail.com>
2017-04-12 11:23:20 +0800

    upd: 优化博客markdown优化

--- static/js/common/post-detail.js
+++ static/js/common/post-detail.js
@@ -1,38 +1,27 @@
-define(['zepto', 'reveal', 'head', 'marked', 'highlight'],
-  function($, Reveal, head, marked, highlight) {
+define(['zepto', 'reveal', 'head'],
+  function($, Reveal, head) {
 
     function postDetail() {
-      this.$postMarkdown = $('#postMarkdown');
       this.$postContainer = $('#postContainer');
       this.$commentsListContainer = $('#postCommentsList');
       this.$commentsFormContainer = $('#postCommentsForm');
-      this.marked = this.setMarked();
-
-      this.init();
+      this.issues_id = window.CONSTANT.issues_id;
     }
 
     postDetail.prototype = {
-      init: function() {
-        this.markdown = this.$postMarkdown.val();
-        this.html = this.marked(this.markdown);
-        this.issues_id = window.CONSTANT.issues_id;
-      },
-      setMarked: function() {
-        // Synchronous highlighting with highlight.js
-        marked.setOptions({
-          highlight: function(code) {
-            return hljs.highlightAuto(code).value;
-          }
-        });
-        return marked;
-      },
       render: function() {
         this.renderPost();
         this.renderCommentsList();
         this.renderCommentsForm();
       },
       renderPost: function() {
-        this.$postContainer.html(this.html);
+        // github 代码高亮的逻辑是：如果``` ```语法中有语言声明，则高亮；
+        // 这里先把高亮的方案干掉
+        // 
+        // 代码高亮
+        // this.$postContainer.find('pre code').each(function(i, block) {
+        //   hljs.highlightBlock(block);
+        // });
       },
       renderCommentsForm: function() {
         var me = this;
@@ -107,21 +96,8 @@ define(['zepto', 'reveal', 'head', 'marked', 'highlight'],
 
           me.$postCommentsList = me.$commentsListContainer.children('.post-comments-list');
 
-          me.setCommentsListDefault();
           me.bindCommentsListEvent();
         });
-      },
-      setCommentsListDefault: function() {
-        var me = this;
-
-        var $commentsContentList = me.$postCommentsList.find('.comments-detail-content');
-
-        for (var i = 0; i < $commentsContentList.length; i++) {
-          var $commentsContent = $($commentsContentList[i]);
-          var markdown = $commentsContent.children('.comments-detail-markdown').val();
-
-          $commentsContent.html(me.marked(markdown))
-        }
       },
       bindCommentsListEvent: function() {
         var me = this;
